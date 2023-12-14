e4668b16abf6a0e9567f5a4b6028c8f0f9b54f8a
Harpreet Singh <3ddeveloper21@gmail.com>
2023-12-13 20:29:24 -0500

    GLSL Vertex Shader created but not attached in WebGL

--- myWebGL.js
+++ myWebGL.js
@@ -2,7 +2,39 @@
     Objective: Create a triangle on the screen.
 */
 
-// Get HTML Canvas object
-const canvas = document.getElementById('#c');
+function drawTriangle()
+{
+    // Get HTML Canvas object
+    const canvas = document.getElementById('c');
 
-console.log(canvas);
+    // check if there is a canvas object - if no canvasd object then break out of drawTriangle
+    if(!canvas) {console.log("No Canvas Object"); return;}
+
+    // create WebGL Context
+    const gl = canvas.getContext('webgl');
+
+    // check if WebGL Context is supported
+    if(!gl) { console.log("WebGL2 is not Supported"); return; }
+
+    // Define data GPU will use - Vertex data
+    const vertices = 
+    [
+        // Top middle
+        0.0, 0.5,
+        
+    ]
+}
+
+// try and catch for errors
+try 
+{
+    drawTriangle();
+} 
+catch(err)
+{
+    console.log(err);
+}
+
+    
+
+
